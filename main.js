498e81707de85c66378d11fe15e7dd6d23b892c8
mucahidertugrul <109730483+mucahidertugrul@users.noreply.github.com>
2022-10-06 14:58:52 +0300

    sub added

--- main.js
+++ main.js
@@ -18,10 +18,14 @@ increaseButton.forEach((item)=>{
         console.log('price =>',price)
         let total = price * count
         console.log('total => ' , total)
-        item.parentElement.nextElementSibling.nextElementSibling.children[1].innerText = '$' + total
+        item.parentElement.nextElementSibling.nextElementSibling.children[1].innerText = '$' + total.toFixed(2)
         let productTotal = item.parentElement.nextElementSibling.nextElementSibling.children[1].innerText
         productTotal=productTotal.slice(1)
         console.log('productTotal => ',productTotal)
+        let subTotal = document.querySelector('.one').children[1].innerText.slice(0,-1)
+        console.log('subTotal => ' , subTotal)
+        subTotal = parseFloat(subTotal) + parseFloat(price)
+        document.querySelector('.one').children[1].innerText = subTotal.toFixed(2) + '$' 
     } )
 })
 
@@ -42,12 +46,20 @@ decreaseButton.forEach((item) => {
         let productTotal = item.parentElement.nextElementSibling.nextElementSibling.children[1].innerText
         productTotal = productTotal.slice(1)
         console.log('productTotal => ' , productTotal)
+        let subTotal = document.querySelector('.one').children[1].innerText.slice(0,-1)
+        console.log('subTotal => ' , subTotal)
+        subTotal = parseFloat(subTotal) - parseFloat(price)
+        document.querySelector('.one').children[1].innerText = subTotal.toFixed(2) + '$'
     })
 })
 
 removeButton.forEach((item) => {
     item.addEventListener( 'click' , () => {
-        let cancel = 0
+         let productTotal0 =item.parentElement.nextElementSibling.children[1].innerText.slice(1)
+         console.log('productTotal0 => ' , productTotal0)
+         let subTotal = document.querySelector('.one').children[1].innerText.slice(0,-1)
+         document.querySelector('.one').children[1].innerText = (parseFloat(subTotal) - parseFloat(productTotal0)).toFixed(2) + '$'
+         let cancel = 0
         console.log('cancel => ' , cancel)
         let price = item.parentElement.nextElementSibling.children [1].innerText
         price = price.slice(1) 
@@ -61,17 +73,13 @@ removeButton.forEach((item) => {
         console.log('productTotal => ' , productTotal)
         item.parentElement.previousElementSibling.children [1].innerText = result
         count = item.parentElement.previousElementSibling.children [1].innerText
-        count = count
         console.log('count => ' , count)
-         
     } )
 } )
 
-
-
-// decrease btn
-// remove btn
-// subtotal
-
-// shipping 1$
+// shipping parça başı 1$
 // tax*%18
+
+// tax her event de yenilenecek
+// shipping her event de yenilenecek
+// total her event de yenilelecek
